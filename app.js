fdd004041d5c0afa2c089ead149681119075a3e9
tuan tang <tuan.ps4u@gmail.com>
2020-06-22 10:06:40 +0700

    update server

--- app.js
+++ app.js
@@ -31,7 +31,6 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(rateLimiter);
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -50,9 +49,11 @@ app.use("/", registerRouter);
 app.use("/", verifyRouter);
 app.use("/", changePasswordRouter);
 app.use("/", forgotPasswordRouter);
-app.get(/.*/, function (req, res) {
-  res.sendFile(__dirname + "/dist/index.html");
-});
+// app.get(/.*/, function (req, res) {
+//   res.sendFile(__dirname + "/dist/index.html");
+// });
+
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
